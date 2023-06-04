b9e37df510c33b7fc5f831851ce94a4820c67a3d
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-06-04 18:31:44 -0300

    chore: add template generator script

---
+++ genjin.py
@@ -0,0 +1,43 @@
+"""genjin.py - Generate HTML from Jinja2 templates
+
+Usage:
+    genjin.py [-h | --help]
+    genjin.py [options] <template>
+
+Options:
+    -o, --output=<output>  Choose output file
+"""
+import sys
+from datetime import datetime
+
+from jinja2 import Environment, FileSystemLoader
+from docopt import docopt
+
+
+options = docopt(__doc__)
+
+
+template_dir = "./"
+env = Environment(loader=FileSystemLoader(template_dir))
+
+
+if options["<template>"] is None:
+    print("please provide a template file", file=sys.stderr)
+    sys.exit(1)
+else:
+    template_file = options["<template>"]
+
+
+date = datetime.today().strftime("%B %d, %Y")
+iso_date = datetime.today().date().isoformat()
+template = env.get_template(template_file)
+rendered_html = template.render(date=date, iso_date=iso_date)
+
+
+output_file = options["--output"]
+if output_file is None:
+    print(rendered_html)
+else:
+    with open(output_file, mode="w") as file:
+        file.write(rendered_html)
+        file.write("\n")
